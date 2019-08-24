b84bd2963719bac0f4072e7c382752ebfdaf6faf
John Hayduk <jhayduk@cisco.com>
2019-08-24 15:58:26 -0400

    Provide some crude collision detection for movement.

--- link.py
+++ link.py
@@ -53,12 +53,12 @@ class Link(object):
             # Link's topleft corner ends up in the center instead of him being dead
             # center at the start, but the tiles around are clear and this works
             # just fine.
-            self.__topleft = pylink_config.PYLINK_MAP.center
             self.__facing = "down"
             self.__moving = False
             self.__step = 0
             self.__current_subsurface = self.__get_facing_down_subsurface(
                 self.__step)
+            self.__rect = pygame.Rect(pylink_config.PYLINK_MAP.center, self.__current_subsurface.get_size())
             self.__velocity = pylink_config.LINK_STOPPED_VELOCITY
             Link.__instance = self
 
@@ -66,10 +66,12 @@ class Link(object):
     def __get_facing_left_subsurface(self, step):
         """
         Return the subsurface that contains Link facing left.
+        This gets called only when Link switches to facing in this direction,
+        or when he takes a step.
         """
         if self.__facing_left_subsurface[0] is None:
             self.__facing_left_subsurface[0] = self.__sprite_sheet.subsurface(
-                pygame.Rect(pylink_config.scale_nes_tuple_to_pylink((35, 11)), pylink_config.scale_nes_tuple_to_pylink((15, 16))))
+                pygame.Rect(pylink_config.scale_nes_tuple_to_pylink((35, 11)), pylink_config.scale_nes_tuple_to_pylink((16, 16))))
             self.__facing_left_subsurface[0].set_colorkey(
                 self.__facing_left_subsurface[0].get_at((0, 0)))
             # The image in the sprite sheet is actually facing right, flip
@@ -78,9 +80,10 @@ class Link(object):
                 self.__facing_left_subsurface[0], True, False)
             # Turn off alpha so that colorkey works for transparency
             self.__facing_left_subsurface[0].set_alpha(None)
+
         if self.__facing_left_subsurface[1] is None:
             self.__facing_left_subsurface[1] = self.__sprite_sheet.subsurface(
-                pygame.Rect(pylink_config.scale_nes_tuple_to_pylink((52, 12)), pylink_config.scale_nes_tuple_to_pylink((14, 15))))
+                pygame.Rect(pylink_config.scale_nes_tuple_to_pylink((52, 12)), pylink_config.scale_nes_tuple_to_pylink((15, 15))))
             self.__facing_left_subsurface[1].set_colorkey(
                 self.__facing_left_subsurface[1].get_at((0, 0)))
             # The image in the sprite sheet is actually facing right, flip
@@ -95,6 +98,8 @@ class Link(object):
     def __get_facing_up_subsurface(self, step):
         """
         Return the subsurface that contains Link facing up.
+        This gets called only when Link switches to facing in this direction,
+        or when he takes a step.
         """
         for index, offset in enumerate([(69, 11), (86, 11)]):
             if self.__facing_up_subsurface[index] is None:
@@ -110,17 +115,19 @@ class Link(object):
     def __get_facing_right_subsurface(self, step):
         """
         Return the subsurface that contains Link facing right.
+        This gets called only when Link switches to facing in this direction,
+        or when he takes a step.
         """
         if self.__facing_right_subsurface[0] is None:
             self.__facing_right_subsurface[0] = self.__sprite_sheet.subsurface(
-                pygame.Rect(pylink_config.scale_nes_tuple_to_pylink((35, 11)), pylink_config.scale_nes_tuple_to_pylink((15, 16))))
+                pygame.Rect(pylink_config.scale_nes_tuple_to_pylink((35, 11)), pylink_config.scale_nes_tuple_to_pylink((16, 16))))
             self.__facing_right_subsurface[0].set_colorkey(
                 self.__facing_right_subsurface[0].get_at((0, 0)))
             # Turn off alpha so that colorkey works for transparency
             self.__facing_right_subsurface[0].set_alpha(None)
         if self.__facing_right_subsurface[1] is None:
             self.__facing_right_subsurface[1] = self.__sprite_sheet.subsurface(
-                pygame.Rect(pylink_config.scale_nes_tuple_to_pylink((52, 12)), pylink_config.scale_nes_tuple_to_pylink((14, 15))))
+                pygame.Rect(pylink_config.scale_nes_tuple_to_pylink((52, 12)), pylink_config.scale_nes_tuple_to_pylink((15, 15))))
             self.__facing_right_subsurface[1].set_colorkey(
                 self.__facing_right_subsurface[1].get_at((0, 0)))
             # Turn off alpha so that colorkey works for transparency
@@ -131,6 +138,8 @@ class Link(object):
     def __get_facing_down_subsurface(self, step):
         """
         Return the subsurface that contains Link facing down.
+        This gets called only when Link switches to facing in this direction,
+        or when he takes a step.
         """
         for index, offset in enumerate([(1, 11), (18, 11)]):
             if self.__facing_down_subsurface[index] is None:
@@ -151,6 +160,10 @@ class Link(object):
         self.__velocity = pylink_config.LINK_MOVE_LEFT_VELOCITY
         self.__current_subsurface = self.__get_facing_left_subsurface(
             self.__step)
+        # Not every image of Link is the same size, so recalculate the
+        # his bounding rectangle after possibly switching facing direction.
+        self.__rect = pygame.Rect(
+            self.__rect.topleft, self.__current_subsurface.get_size())
 
     def up_keydown(self):
         """
@@ -161,6 +174,10 @@ class Link(object):
         self.__velocity = pylink_config.LINK_MOVE_UP_VELOCITY
         self.__current_subsurface = self.__get_facing_up_subsurface(
             self.__step)
+        # Not every image of Link is the same size, so recalculate the
+        # his bounding rectangle after possibly switching facing direction.
+        self.__rect = pygame.Rect(
+            self.__rect.topleft, self.__current_subsurface.get_size())
 
     def right_keydown(self):
         """
@@ -171,6 +188,10 @@ class Link(object):
         self.__velocity = pylink_config.LINK_MOVE_RIGHT_VELOCITY
         self.__current_subsurface = self.__get_facing_right_subsurface(
             self.__step)
+        # Not every image of Link is the same size, so recalculate the
+        # his bounding rectangle after possibly switching facing direction.
+        self.__rect = pygame.Rect(
+            self.__rect.topleft, self.__current_subsurface.get_size())
 
     def down_keydown(self):
         """
@@ -181,6 +202,10 @@ class Link(object):
         self.__velocity = pylink_config.LINK_MOVE_DOWN_VELOCITY
         self.__current_subsurface = self.__get_facing_down_subsurface(
             self.__step)
+        # Not every image of Link is the same size, so recalculate the
+        # his bounding rectangle after possibly switching facing direction.
+        self.__rect = pygame.Rect(
+            self.__rect.topleft, self.__current_subsurface.get_size())
 
     def arrow_keyup(self):
         """
@@ -198,22 +223,101 @@ class Link(object):
         "right": lambda self, step: self.__get_facing_right_subsurface(step)  # pylint: disable=protected-access
     }
 
+    def __it_is_ok_to_move(self, fromRect, toRect):
+        """
+        Determine if it is valid to move the Link rectangle from the fromRect
+        to the toRect locations on the current map.
+        Return True if it is OK, and False if not.
+
+        Right now this is cheating a bit and using the color of the
+        pixels at corners of Link's bounding rectangle in the direction of
+        movement and comparing them with the ones at the new location.
+        If they are the same, it is assumed that it must be OK to move there,
+        because it is, apparanetly, just as OK to stay in this position. This
+        does assume that the pixel at that location on the surface is the
+        color of the pixel in the background which means that the pixel
+        has to be transparent on Link's image. Because of this, only some
+        directions check the midpoint.
+        """
+        # Get the color of the pixel at Link's current  and next locations on
+        # the corners facing the move
+        if self.__facing == "left":
+            current_locations_colors = (
+                pygame.display.get_surface().get_at(fromRect.topleft),
+                pygame.display.get_surface().get_at(fromRect.midleft),
+                pygame.display.get_surface().get_at(fromRect.bottomleft)
+            )
+            next_locations_colors = (
+                pygame.display.get_surface().get_at(toRect.topleft),
+                pygame.display.get_surface().get_at(toRect.midleft),
+                pygame.display.get_surface().get_at(toRect.bottomleft)
+            )
+        elif self.__facing == "up":
+            current_locations_colors = (
+                pygame.display.get_surface().get_at(fromRect.topleft),
+                pygame.display.get_surface().get_at(fromRect.topright)
+            )
+            next_locations_colors = (
+                pygame.display.get_surface().get_at(toRect.topleft),
+                pygame.display.get_surface().get_at(toRect.topright)
+            )
+        elif self.__facing == "right":
+            current_locations_colors = (
+                pygame.display.get_surface().get_at(fromRect.topright),
+                pygame.display.get_surface().get_at(fromRect.midright),
+                pygame.display.get_surface().get_at(fromRect.bottomright)
+            )
+            next_locations_colors = (
+                pygame.display.get_surface().get_at(toRect.topright),
+                pygame.display.get_surface().get_at(toRect.midright),
+                pygame.display.get_surface().get_at(toRect.bottomright)
+            )
+        elif self.__facing == "down":
+            current_locations_colors = (
+                pygame.display.get_surface().get_at(fromRect.bottomleft),
+                pygame.display.get_surface().get_at(fromRect.midbottom),
+                pygame.display.get_surface().get_at(fromRect.bottomright)
+            )
+            next_locations_colors = (
+                pygame.display.get_surface().get_at(toRect.bottomleft),
+                pygame.display.get_surface().get_at(toRect.midbottom),
+                pygame.display.get_surface().get_at(toRect.bottomright)
+            )
+        else:
+            raise Exception("Unknown facing direction: '" + self.__facing + "'");
+
+        # Return whether they are the same or not
+        return current_locations_colors == next_locations_colors
+
     def move(self):
         """
         If Link is moving, toggle the __step setting, set the
         __current_surface to the correct one with the new step,
-        and shift the location set by __topleft
+        and shift the location
         """
         if self.__moving:
+            # Change the step image.
             if self.__step == 0:
                 self.__step = 1
             else:
                 self.__step = 0
             self.__current_subsurface = self.__toggle_steps_switcher.get(self.__facing, lambda self, step: print("Unknown facing direction: ", self.__facing))(self, self.__step)
-            self.__topleft = tuple(numpy.add(self.__topleft, self.__velocity))
+
+            # Not every image of Link is the same size, so recalculate the
+            # his bounding rectangle after taking the step
+            self.__rect = pygame.Rect(
+                self.__rect.topleft, self.__current_subsurface.get_size())
+
+            # Calculate the bounding rectangle of the planned next location
+            next_rect = self.__rect.move(self.__velocity)
+
+            # See if it is clear to move to that next location and
+            # move if it is clear to do so
+            if self.__it_is_ok_to_move(self.__rect, next_rect):
+                self.__rect = self.__rect.move(self.__velocity)
 
     def blit(self):
         """
         Blits Link to his current location on the screen.
         """
-        pygame.display.get_surface().blit(self.__current_subsurface, self.__topleft)
+        pygame.display.get_surface().blit(self.__current_subsurface, self.__rect.topleft)
