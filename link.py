cfe4223afdfc00e7cdf3b81ea7265c5b3370da9f
John Hayduk <jhayduk@cisco.com>
2023-12-25 08:22:27 -0500

    Add the ability for Link to wrap around the current map when walking either left or right.  This is a precursor to being able to switch maps. Also, start renaming properties and methods to remove the leading underscores. While they serve a purpose, they clutter the code and make it feel less readable.

--- link.py
+++ link.py
@@ -6,6 +6,17 @@ import pygame
 import numpy
 import pylink_config
 
+
+def should_switch_maps(next_rect):
+    """
+    Check to see if next_rect would be off the current map, and return
+    true if it is. This would indicate that the map needs to be
+    switched to a new one.
+    """
+    current_map = pygame.display.get_surface().get_rect()
+    return not current_map.contains(next_rect)
+
+
 class Link(object):
     """
     The Link player object.
@@ -54,7 +65,7 @@ class Link(object):
             # Link's top left corner ends up in the center instead of him being dead
             # center at the start, but the tiles around are clear and this works
             # just fine.
-            self.__facing = "down"
+            self.facing_direction = "down"
             self.__moving = False
             self.__step = 0
             self.__facing_left_subsurface = [None, None]
@@ -69,8 +80,8 @@ class Link(object):
 
     def __get_facing_left_subsurface(self, step):
         """
-        Return the subsurface that contains Link facing left.
-        This gets called only when Link switches to facing in this direction,
+        Return the subsurface that contains Link facing_direction left.
+        This gets called only when Link switches to facing_direction in this direction,
         or when he takes a step.
         """
         if self.__facing_left_subsurface[0] is None:
@@ -78,7 +89,7 @@ class Link(object):
                 pygame.Rect(pylink_config.scale_nes_tuple_to_pylink((35, 11)), pylink_config.scale_nes_tuple_to_pylink((16, 16))))
             self.__facing_left_subsurface[0].set_colorkey(
                 self.__facing_left_subsurface[0].get_at((0, 0)))
-            # The image in the sprite sheet is actually facing right, flip
+            # The image in the sprite sheet is actually facing_direction right, flip
             # it to face left
             self.__facing_left_subsurface[0] = pygame.transform.flip(
                 self.__facing_left_subsurface[0], True, False)
@@ -90,7 +101,7 @@ class Link(object):
                 pygame.Rect(pylink_config.scale_nes_tuple_to_pylink((52, 12)), pylink_config.scale_nes_tuple_to_pylink((15, 15))))
             self.__facing_left_subsurface[1].set_colorkey(
                 self.__facing_left_subsurface[1].get_at((0, 0)))
-            # The image in the sprite sheet is actually facing right, flip
+            # The image in the sprite sheet is actually facing_direction right, flip
             # it to face left
             self.__facing_left_subsurface[1] = pygame.transform.flip(
                 self.__facing_left_subsurface[1], True, False)
@@ -100,8 +111,8 @@ class Link(object):
 
     def __get_facing_up_subsurface(self, step):
         """
-        Return the subsurface that contains Link facing up.
-        This gets called only when Link switches to facing in this direction,
+        Return the subsurface that contains Link facing_direction up.
+        This gets called only when Link switches to facing_direction in this direction,
         or when he takes a step.
         """
         for index, offset in enumerate([(69, 11), (86, 11)]):
@@ -116,8 +127,8 @@ class Link(object):
 
     def __get_facing_right_subsurface(self, step):
         """
-        Return the subsurface that contains Link facing right.
-        This gets called only when Link switches to facing in this direction,
+        Return the subsurface that contains Link facing_direction right.
+        This gets called only when Link switches to facing_direction in this direction,
         or when he takes a step.
         """
         if self.__facing_right_subsurface[0] is None:
@@ -138,8 +149,8 @@ class Link(object):
 
     def __get_facing_down_subsurface(self, step):
         """
-        Return the subsurface that contains Link facing down.
-        This gets called only when Link switches to facing in this direction,
+        Return the subsurface that contains Link facing_direction down.
+        This gets called only when Link switches to facing_direction in this direction,
         or when he takes a step.
         """
         for index, offset in enumerate([(1, 11), (18, 11)]):
@@ -156,13 +167,13 @@ class Link(object):
         """
         This method is called when the left arrow key is pressed.
         """
-        self.__facing = "left"
+        self.facing_direction = "left"
         self.__moving = True
         self.__velocity = pylink_config.LINK_MOVE_LEFT_VELOCITY
         self.__current_subsurface = self.__get_facing_left_subsurface(
             self.__step)
         # Not every image of Link is the same size, so recalculate
-        # his bounding rectangle after possibly switching facing direction.
+        # his bounding rectangle after possibly switching facing_direction direction.
         self.__rect = pygame.Rect(
             self.__rect.topleft, self.__current_subsurface.get_size())
 
@@ -170,13 +181,13 @@ class Link(object):
         """
         This method is called when the up arrow key is pressed.
         """
-        self.__facing = "up"
+        self.facing_direction = "up"
         self.__moving = True
         self.__velocity = pylink_config.LINK_MOVE_UP_VELOCITY
         self.__current_subsurface = self.__get_facing_up_subsurface(
             self.__step)
         # Not every image of Link is the same size, so recalculate
-        # his bounding rectangle after possibly switching facing direction.
+        # his bounding rectangle after possibly switching facing_direction direction.
         self.__rect = pygame.Rect(
             self.__rect.topleft, self.__current_subsurface.get_size())
 
@@ -184,13 +195,13 @@ class Link(object):
         """
         This method is called when the right arrow key is pressed.
         """
-        self.__facing = "right"
+        self.facing_direction = "right"
         self.__moving = True
         self.__velocity = pylink_config.LINK_MOVE_RIGHT_VELOCITY
         self.__current_subsurface = self.__get_facing_right_subsurface(
             self.__step)
         # Not every image of Link is the same size, so recalculate
-        # his bounding rectangle after possibly switching facing direction.
+        # his bounding rectangle after possibly switching facing_direction direction.
         self.__rect = pygame.Rect(
             self.__rect.topleft, self.__current_subsurface.get_size())
 
@@ -198,13 +209,13 @@ class Link(object):
         """
         This method is called when the down arrow key is pressed.
         """
-        self.__facing = "down"
+        self.facing_direction = "down"
         self.__moving = True
         self.__velocity = pylink_config.LINK_MOVE_DOWN_VELOCITY
         self.__current_subsurface = self.__get_facing_down_subsurface(
             self.__step)
         # Not every image of Link is the same size, so recalculate
-        # his bounding rectangle after possibly switching facing direction.
+        # his bounding rectangle after possibly switching facing_direction direction.
         self.__rect = pygame.Rect(
             self.__rect.topleft, self.__current_subsurface.get_size())
 
@@ -216,7 +227,7 @@ class Link(object):
         self.__velocity = pylink_config.LINK_STOPPED_VELOCITY
 
     # Used to simulate a switch statement for move.
-    # This is indexed by self.__facing
+    # This is indexed by self.facing_direction
     __toggle_steps_switcher = {
         "left": lambda self, step: self.__get_facing_left_subsurface(step),  # pylint: disable=protected-access
         "up": lambda self, step: self.__get_facing_up_subsurface(step),  # pylint: disable=protected-access
@@ -224,7 +235,7 @@ class Link(object):
         "right": lambda self, step: self.__get_facing_right_subsurface(step)  # pylint: disable=protected-access
     }
 
-    def __it_is_ok_to_move(self, to_rect):
+    def can_move_to(self, to_rect):
         """
         Determine if it is valid to move the Link rectangle
         to the to_rect location on the current map.
@@ -232,7 +243,9 @@ class Link(object):
         If an IndexError occurs at any point in the calculations,
         assume it is not safe to move and return False
 
-        Right now this is cheating a bit and using the color of the
+        Right now this is cheating a bit. It turns out that the portion of
+        the overworld map that Link can move on is almost always sand and
+        is the same color, For now, the code is using the color of the
         pixels at corners of Link's current bounding rectangle in the
         direction of movement and comparing them with the ones at the
         proposed new location. If they are the same, it is assumed that it
@@ -244,53 +257,77 @@ class Link(object):
         others do not.
         """
         # Get the color of the pixel at Link's current location and at the
-        # proposed next location on the corners facing the move
+        # proposed next location on the corners facing_direction the move
+        game_window = pygame.display.get_surface()
         try:
-            if self.__facing == "left":
+            if self.facing_direction == "left":
                 current_locations_colors = (
-                    pygame.display.get_surface().get_at(self.__rect.midleft),
-                    pygame.display.get_surface().get_at(self.__rect.bottomleft)
+                    game_window.get_at(self.__rect.midleft),
+                    game_window.get_at(self.__rect.bottomleft)
                 )
                 next_locations_colors = (
-                    pygame.display.get_surface().get_at(to_rect.midleft),
-                    pygame.display.get_surface().get_at(to_rect.bottomleft)
+                    game_window.get_at(to_rect.midleft),
+                    game_window.get_at(to_rect.bottomleft)
                 )
-            elif self.__facing == "up":
+            elif self.facing_direction == "up":
                 current_locations_colors = (
-                    pygame.display.get_surface().get_at(self.__rect.midleft),
-                    pygame.display.get_surface().get_at(self.__rect.midright)
+                    game_window.get_at(self.__rect.midleft),
+                    game_window.get_at(self.__rect.midright)
                 )
                 next_locations_colors = (
-                    pygame.display.get_surface().get_at(to_rect.midleft),
-                    pygame.display.get_surface().get_at(to_rect.midright)
+                    game_window.get_at(to_rect.midleft),
+                    game_window.get_at(to_rect.midright)
                 )
-            elif self.__facing == "right":
+            elif self.facing_direction == "right":
                 current_locations_colors = (
-                    pygame.display.get_surface().get_at(self.__rect.midright),
-                    pygame.display.get_surface().get_at(self.__rect.bottomright)
+                    game_window.get_at(self.__rect.midright),
+                    game_window.get_at(self.__rect.bottomright)
                 )
                 next_locations_colors = (
-                    pygame.display.get_surface().get_at(to_rect.midright),
-                    pygame.display.get_surface().get_at(to_rect.bottomright)
+                    game_window.get_at(to_rect.midright),
+                    game_window.get_at(to_rect.bottomright)
                 )
-            elif self.__facing == "down":
+            elif self.facing_direction == "down":
                 current_locations_colors = (
-                    pygame.display.get_surface().get_at(self.__rect.bottomleft),
-                    pygame.display.get_surface().get_at(self.__rect.midbottom),
-                    pygame.display.get_surface().get_at(self.__rect.bottomright)
+                    game_window.get_at(self.__rect.bottomleft),
+                    game_window.get_at(self.__rect.midbottom),
+                    game_window.get_at(self.__rect.bottomright)
                 )
                 next_locations_colors = (
-                    pygame.display.get_surface().get_at(to_rect.bottomleft),
-                    pygame.display.get_surface().get_at(to_rect.midbottom),
-                    pygame.display.get_surface().get_at(to_rect.bottomright)
+                    game_window.get_at(to_rect.bottomleft),
+                    game_window.get_at(to_rect.midbottom),
+                    game_window.get_at(to_rect.bottomright)
                 )
             else:
-                raise Exception("Unknown facing direction: '" + self.__facing + "'")
+                raise Exception("Unknown facing_direction direction: '" + self.facing_direction + "'")
         except IndexError:
             return False
 
         # Return whether they are the same or not
         return current_locations_colors == next_locations_colors
+
+    def switch_maps(self, facing_direction):
+        """
+        This is called from within the move method when we are switching to a
+        new map. The new map will be the one to the facing_direction from the
+        current one. What this means for Link is that he needs to move to the
+        opposite end of the game screen.
+
+        This method only moves where Link will be drawn next (i.e. it updates
+        self.__rect). It does NOT handle getting the tile for the next step
+        because that is expected to have been taken care of in the move method
+        before this one is called.
+
+        This method also does not check for collisions. It is assumed that the
+        caller (the move method) has already verified that this move should be
+        done.
+        """
+        game_window = pygame.display.get_surface().get_rect()
+        if self.facing_direction == "right":
+            self.__rect = self.__rect.move(game_window.left - self.__rect.left, 0)
+        elif self.facing_direction == "left":
+            self.__rect = self.__rect.move(game_window.right - self.__rect.right, 0)
+
 
     def move(self):
         """
@@ -304,7 +341,7 @@ class Link(object):
                 self.__step = 1
             else:
                 self.__step = 0
-            self.__current_subsurface = self.__toggle_steps_switcher.get(self.__facing, lambda self, step: print("Unknown facing direction: ", self.__facing))(self, self.__step)
+            self.__current_subsurface = self.__toggle_steps_switcher.get(self.facing_direction, lambda self, step: print("Unknown facing_direction direction: ", self.facing_direction))(self, self.__step)
 
             # Not every image of Link is the same size, so recalculate
             # his bounding rectangle after taking the step
@@ -315,9 +352,13 @@ class Link(object):
             next_rect = self.__rect.move(self.__velocity)
 
             # See if it is clear to move to the next location and
-            # move if it is clear to do so
-            if self.__it_is_ok_to_move(next_rect):
-                self.__rect = self.__rect.move(self.__velocity)
+            # move if it is clear to do so. If the move would go off an edge
+            # of the map, switch maps and move to the other side of the new
+            # map.
+            if self.can_move_to(next_rect):
+                self.__rect = next_rect
+            elif should_switch_maps(next_rect):
+                self.switch_maps(self.facing_direction)
 
     def blit(self):
         """
