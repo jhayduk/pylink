0ed7abac849bd8f63b1353eff91e09f641b6c9c0
John Hayduk <jhayduk@cisco.com>
2023-12-27 17:41:07 -0500

    Add unit tests for the for the switch_maps method.

--- test_link.py
+++ test_link.py
@@ -1,6 +1,7 @@
 """Tests for link.py"""
 from link import Link
 import pygame
+import pylink_config
 import pytest
 
 
@@ -23,3 +24,36 @@ class TestLinkConstructors:
         first_object = Link.get_instance()
         second_object = Link.get_instance()
         assert first_object is second_object
+
+
+class TestLinkSwitchMaps:
+    """Test for the switch_maps method"""
+
+    @pytest.fixture
+    def init_link(self):
+        """
+        Gets a pointer to the Link singleton instance and then make sure
+        it is positioned in its normal starting position.
+        """
+        self.link = Link.get_instance()
+        self.link._Link__rect = pygame.Rect(384, 456, 45, 48)
+
+    def test_move_right(self, init_link):
+        """Should switch Link to the left edge of the map window"""
+        self.link.switch_maps("right")
+        assert self.link._Link__rect.left == pylink_config.PYLINK_MAP.left
+
+    def test_move_down(self, init_link):
+        """Should switch Link to the top edge of the map window"""
+        self.link.switch_maps("down")
+        assert self.link._Link__rect.top == pylink_config.PYLINK_MAP.top
+
+    def test_move_left(self, init_link):
+        """Should switch Link to the left edge of the map window"""
+        self.link.switch_maps("left")
+        assert self.link._Link__rect.right == pylink_config.PYLINK_MAP.right
+
+    def test_move_up(self, init_link):
+        """Should switch Link to the bottom edge of the map window"""
+        self.link.switch_maps("up")
+        assert self.link._Link__rect.bottom == pylink_config.PYLINK_MAP.bottom
